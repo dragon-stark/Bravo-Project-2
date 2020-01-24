c6f0399be9c6fee7b5cb8a9a5413b0f616aac8cf
Keith Buchanan <Kbuchanan77@yahoo.com>
2020-01-23 20:43:53 -0700

    Created the beginning of the database's columns

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE database employees_db;
+
+USE employees_db;
+
+CREATE TABLE reviews
+(
+    employee_id INT NOT NULL,
+    last name
+    VARCHAR
+    (25) NOT NULL,
+    first name VARCHAR
+    (25) NOT NULL,
+    department VARCHAR
+    (45) NOT NULL,
+    attitute VARCHAR
+    (100),
+    communication VARCHAR
+    (100),
+    efficiency VARCHAR
+    (100),
+    proficiency VARCHAR
+    (100),
+    Reliability VARCHAR
+    (100),
+    PRIMARY KEY
+    (employee_id)
+);
+
+    SELECT *
+    FROM reviews;
