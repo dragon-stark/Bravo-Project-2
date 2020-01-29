cbcd9def4a598034507b276baba5fddcd1e19187
DStark <hindash@email.arizona.edu>
2020-01-28 23:07:44 -0700

    server.js

--- models/index.js
+++ models/index.js
@@ -4,14 +4,19 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 
-
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
+console.log(config);
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(
+    process.env[config.use_env_variable],
+    config.username,
+    config.password,
+    config
+  );
 } else {
   var sequelize = new Sequelize(
     config.database,
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-
 var db = require("./models");
 
 var app = express();
