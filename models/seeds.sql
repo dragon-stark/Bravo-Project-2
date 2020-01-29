d7886145f9024601285cc69cfd86e09047430e54
Keith Buchanan <Kbuchanan77@yahoo.com>
2020-01-28 21:41:36 -0700

    added seeds for employees

---
+++ models/seeds.sql
@@ -0,0 +1,60 @@
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Lebsack', 'Aliyah', 'Safari Guide', 1, 2, 3, 4, 5 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Bayer', 'Helga', 'Safari Guide', 6, 7, 8, 9, 10 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Gutmann', 'Glennie', 'Safari Guide', 11, 12, 13, 14, 15 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Bednar', 'Gennaro', 'Wildlife Expert', 16, 17, 18, 19, 20 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Krajcik', 'Morgan', 'Wildlife Expert', 21, 22, 23, 24, 25 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Hansen', 'Giovanny', 'IT', 26, 27, 28, 29, 30 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Torphy', 'Owen', 'Medic', 31, 32, 33, 34, 35 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Bosco', 'Tristin', 'Front Desk', 36, 37, 38, 39, 40 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Kozey', 'Tessie', 'Front Desk', 41, 42, 43, 44, 45 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Lueilwitz', 'Elena', 'Animal Technician', 46, 47, 48, 49, 50 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Daniel', 'Isadore', 'Animal Technician', 51, 52, 53, 54, 55 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Oberbrunner', 'Ashly', 'Janitor', 56, 57, 58, 59, 60 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Denesik II', 'Kristopher', 'Health', 61, 62, 63, 64, 65 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Wisoky', 'Tanner', 'Health', 66, 67, 68, 69, 70 );
+INSERT INTO reviews
+    (last_name, first_name, department, attitute, communication, efficiency, proficiency, Reliability)
+VALUES
+    ('Wyman', 'Arianna', 'Health', 71, 72, 73, 74, 75 );
