6e24ad86402dd686aa55cc943e16cabb4eaba45d
Daniel Joji Koons <jojikoons@gmail.com>
2020-01-28 18:55:35 -0700

    I hope we don't break this

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,10 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/employees", function(req, res) {
+    res.render("employees");
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
