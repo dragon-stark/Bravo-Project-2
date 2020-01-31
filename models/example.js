ff3adc4f186c6456ad72eb357f0a81b6e7491fe1
Daniel Joji Koons <jojikoons@gmail.com>
2020-01-30 19:13:35 -0700

    MEGA PUSH

--- models/example.js
+++ models/example.js
@@ -1,7 +1,54 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
+  var Employee = sequelize.define(
+    "reviews",
+    {
+      employee_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 25]
+        }
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 25]
+        }
+      },
+      department: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 45]
+        }
+      },
+      attitute: {
+        type: DataTypes.INTEGER
+      },
+      communication: {
+        type: DataTypes.INTEGER
+      },
+      efficiency: {
+        type: DataTypes.INTEGER
+      },
+      proficiency: {
+        type: DataTypes.INTEGER
+      },
+      Reliability: {
+        type: DataTypes.INTEGER
+      }
+    },
+    {
+      tableName: "reviews",
+      timestamps: false
+    }
+  );
+  return Employee;
 };
